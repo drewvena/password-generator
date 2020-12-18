b892c3cfb30a1c7814b86399d718e7b35898ab1e
Drew Vena <drewvena1@gmail.com>
2020-12-17 19:31:17 -0600

    added if statements, working on for statement

--- develop/script.js
+++ develop/script.js
@@ -2,7 +2,7 @@
 
 
 function generatePassword(){
-var passwordLength = Number(prompt ("How many characters? between 8 and 128"))
+var passwordLength = prompt ("How many characters? between 8 and 128");
 alert ('you have chosen ' + passwordLength);
 var numbers = confirm ("Do you want numbers in your password?");
 alert (`You Have Chosen ${numbers}`);
@@ -12,7 +12,28 @@ var upper = confirm ("Do you want uppercaser letters?");
 alert ("You have chosen " + upper)
 var special = confirm ("Do you want Special Characters?");
 alert ("You have chosen " + special)
+
+var charSet = "";
+if (numbers=true){
+  charSet = "0123456789";
+}
+ if(lower =true){
+  charSet="abcdefghijklmnopqrstuvwxyz";
+}  
+if (upper=true){
+  charSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+if (special=true){
+  charSet= "!@#$%^&*()_-+=}{[]\`|~?/;:'";
 };
+var value = "";
+for (var i = 0; i < passwordLength; i++) { 
+ value += charSet[Math.floor(Math.random()* charSet.length)];
+}
+return value;
+}
+alert(generatePassword()); 
+generatePassword();
 
 
 
