a3d8ab5d7dcf51b461345c90aac4003d39534f9c
Drew Vena <drewvena1@gmail.com>
2020-12-20 13:18:00 -0600

    added else if to the confirms

--- develop/script.js
+++ develop/script.js
@@ -1,17 +1,14 @@
 // Assignment code here
 var specialCharacters = [
-  '@','%','+','\\','/',"'",'!','#','$','^','?',':',')','(','}','{',']','[','~','-','_','.'
-];
+  "@","%","+","/","!","#","$","^","?"];
 
 var upperCasedCharacters = [
-  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-];
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 var lowerCasedCharacters = [
-  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-];
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = [0,1,2,3,4,5,6,7,8,9];
 
 var selectedCharacters = [];
 function generatePassword(){
@@ -25,41 +22,43 @@ var upperCase = confirm("Use Upper case characters?");
 var numbers = confirm("Use Numeric characters?");
 var special = confirm("Use Special characters?");
 
-if (lowerCase == true || upperCase == true || numbers == true || special == true){
+if (lowerCase == true || upperCase ==true || numbers == true || special == true){
 
   if (lowerCase = true) {
     selectedCharacters += lowerCasedCharacters;
-  } else {
-    selectedCharacters=selectedCharacters;
-  }
+  } else if (lowerCase==false) {
+    selectedCharacters=upperCasedCharacters,specialCharacters,numericCharacters;
+  };
 
   if (upperCase = true) {
     selectedCharacters += upperCasedCharacters;
-  }else {
-    selectedCharacters=selectedCharacters;
-  }
+  }else if (upperCase==false){
+    selectedCharacters=lowerCasedCharacters,numericCharacters,specialCharacters;
+  };
 
   if (numbers == true) {
     selectedCharacters += numericCharacters;
-  }else {
-    selectedCharacters=selectedCharacters;
-  }
+  }else if (numbers==false){
+    selectedCharacters=upperCasedCharacters+specialCharacters+upperCasedCharacters;
+  };
 
   if (special == true) {
     selectedCharacters += specialCharacters;
-  }else {
-    selectedCharacters=selectedCharacters;
-  }
+  }else if (special==false){
+    selectedCharacters=numericCharacters+=upperCasedCharacters+=lowerCasedCharacters;
+  };
+  
 
 console.log(selectedCharacters);
 var retVal = "";
 for (var i = 0; i < passwordLength; i++) { 
- retVal += selectedCharacters.charAt(Math.floor(Math.random()* selectedCharacters.length));
+ retVal += selectedCharacters.charAt (Math.floor(Math.random()* selectedCharacters.length));
 }
 return retVal;
 }
-alert(generatePassword());
-}
+generatePassword;
+};
+
 
 
 
